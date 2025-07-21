545071a7d12de94684c0169b56f660fc89e26930
bostang <bostangsteiitb2020@gmail.com>
2025-07-21 10:43:49 +0700

    change mode  to executable add_submodules.sh

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,7 +1,8 @@
 # adding submodules of project:
-git submodule add https://github.com/bostang/verificator-secure-onboarding-system
-git submodule add https://github.com/bostang/backend-secure-onboarding-system
-git submodule add https://github.com/alvarolt17/frontend-secure-onboarding-system
+# JALANKAN INI KETIKA BELUM ADA FILE .gitmodules
+# git submodule add https://github.com/bostang/verificator-secure-onboarding-system
+# git submodule add https://github.com/bostang/backend-secure-onboarding-system
+# git submodule add https://github.com/alvarolt17/frontend-secure-onboarding-system
 
 # pull down the contents of the submodules
 git submodule update --init --recursive
