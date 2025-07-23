f8c27e686b387da41ba1d5eecba1492b304d1879
bostang <bostangsteiitb2020@gmail.com>
2025-07-23 13:14:39 +0700

    update port in docker-compose.yml

--- add_submodules.sh
+++ add_submodules.sh
@@ -3,6 +3,7 @@
 # git submodule add https://github.com/bostang/verificator-secure-onboarding-system
 # git submodule add https://github.com/bostang/backend-secure-onboarding-system
 # git submodule add https://github.com/alvarolt17/frontend-secure-onboarding-system
+# git submodule add https://github.com/qanitasyaf/ops-secure-onboarding-system/
 
 # pull down the contents of the submodules
 git submodule update --init --recursive
